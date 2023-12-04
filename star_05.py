84f4aa0d7210cba01c97d2874bd9c85f3e9aae19
Poirier <8902046+gmPoirier@users.noreply.github.com>
2023-12-03 20:44:37 -0600

    Adds skeleton star_05.py file

---
+++ star_05.py
@@ -0,0 +1,11 @@
+# Advent of Code 2023: Star 05
+#=============================
+# Given an file with lines of text, ouput a single integer
+
+def main():
+    file = open("input/day_03.txt", "r", encoding="utf-8")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
