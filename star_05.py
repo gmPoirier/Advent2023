727a52567e58b7c1b7b5b79a148049bf949136c8
Poirier <8902046+gmPoirier@users.noreply.github.com>
2023-12-05 22:48:01 -0600

    Adds loop to search for numbers in the previous line with the same index as a symbol in the current line

--- star_05.py
+++ star_05.py
@@ -6,8 +6,9 @@ import re
 def main():
     file = open("input/day_03.txt", "r", encoding="utf-8")
     num = re.compile('[0-9]+')
-    sym = re.compile('[^a-zA-Z0-9\.]+')
+    sym = re.compile('[^a-zA-Z0-9\.\n]+')
     prev = ""
+    out = 0
 
     # I have realized I can just keep track of the current and previous
     # line and use a regex to find any all digit substrings and then get
@@ -15,9 +16,7 @@ def main():
     # line for symbols. Similarly, I can search a string for symbols and
     # Then check the previous string for any digits an the proper indices
     for line in file:
-        if prev == "":
-            prev = line
-
+        line_out = ""
         numbers = num.finditer(line)
         for match in numbers:
 
@@ -29,10 +28,40 @@ def main():
                 start = loc[0] - 1
             if loc[1] < len(line) - 1:
                 end = loc[1] + 1
-            if sym.search(prev, start, end):
-                print(match.group(0))
+
+            if prev != "":
+                if sym.search(prev, start, end):
+                    line_out += match.group(0) + " "
+                    out += int(match.group(0))
+            if sym.search(line, start, end):
+                line_out += match.group(0) + " "
+                out += int(match.group(0))
+
+        symbols = sym.finditer(line)
+        for match in symbols:
+
+            loc = match.span()
+            start = loc[0]
+            end = loc[1]
+
+            if loc[0] > 0:
+                start = loc[0] - 1
+            if loc[1] < len(line) - 1:
+                end = loc[1] + 1
+
+            if prev != "":
+                if num.search(prev, start, end):
+                    prev_nums = num.finditer(prev)
+                    for number in prev_nums:
+                        loc = number.span()
+                        if start < loc[1] and end > loc[0]:
+                            line_out += number.group(0) + " "
+                            out += int(number.group(0))
 
         prev = line
+        print(line_out)
+
+    print(out)
 
     file.close
     return 0
