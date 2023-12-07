871ef585191bd0940f8ecbfe6390b92c748ebce4
Poirier <8902046+gmPoirier@users.noreply.github.com>
2023-12-06 19:26:50 -0600

    Implements parser for input

--- star_07.py
+++ star_07.py
@@ -2,22 +2,34 @@
 #=============================
 
 import math
+import re
 
 def parse(line):
+    num = re.compile("[0-9]+")
+
     card_id = -1
     winning_numbers = []
     my_numbers = []
 
+    numbers_split = line.split("|")
+    my_numbers = num.findall(numbers_split[1])
+
+    id_split = numbers_split[0].split(":")
+    card_id = id_split[0]
+    winning_numbers = num.findall(id_split[1])
+
     return (card_id, winning_numbers, my_numbers)
 
 def count_matches(list_a, list_b):
-    matches = 0
+    matches = []
 
     for item_a in list_a:
         for item_b in list_b:
             if item_a == item_b:
-                matches += 1
-    return matches
+                matches.append(item_a)
+    
+    print(matches)
+    return len(matches)
 
 def main():
     file = open("input/day_04.txt", "r", encoding="utf-8")
@@ -32,7 +44,7 @@ def main():
 
         num_matches = count_matches(winning_numbers, my_numbers)
         if num_matches > 0:
-            total_points = math.pow(2, num_matches - 1)
+            total_points += math.pow(2, num_matches - 1)
 
     print(total_points)
     file.close()
